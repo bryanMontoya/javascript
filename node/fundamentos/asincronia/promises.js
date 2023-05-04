6ad3f141548b359a8262b953c518f67d605992f3
bmontoya <bmontoya@proteccion.com.co>
2023-05-04 16:04:00 -0500

    Promesas

--- node/fundamentos/asincronia/promises.js
+++ node/fundamentos/asincronia/promises.js
@@ -1,42 +1,43 @@
 function hola(nombre){
-    return new Promise(function (resolve, reject) {
+    return new Promise(function(resolve, reject){
         setTimeout(function() {
-            console.log("Hola ", nombre);
+            console.log("Hola " + nombre);
             resolve(nombre);
-        }, 2000);
+        }, 1000)
     });
 }
 
 function hablar(nombre){
-    return new Promise((resolve, reject) => {
-        setTimeout(function(){
-            console.log("Estoy hablando");
+    return new Promise ( (resolve, reject) => {
+        setTimeout(function() {
+            console.log("bla, bla bla");
             //resolve(nombre);
             reject("Hay un error");
         }, 1000);
-    });  
+    })
 }
 
 function adios(nombre){
     return new Promise((resolve, reject) => {
-        setTimeout(function() {
+        setTimeout(function(){
             console.log("Adios " + nombre);
-            resolve();
+            resolve(nombre);
         }, 1000);
-    });
+    })
 }
 
 // ---
-console.log("Iniciando el proceso...");
+console.log("Iniciando el proceso...")
 
-hola("Brayan")
+hola("Carlos")
+    .then(hablar)
     .then(hablar)
     .then(hablar)
     .then(adios)
-    .then((nombre) => {
-        console.log("Terminado el proceso.");
+    .then((nombre) => {  //--Igual a definir una function
+        console.log("Terminado el proceso:" + nombre);
     })
     .catch(error => {
-        console.error("Ha habido un error:" );
+        console.error("Un error parce");
         console.error(error);
     })
