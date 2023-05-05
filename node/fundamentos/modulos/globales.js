f6eb37b6613815048a2c60619fbba49864f5333b
bmontoya <bmontoya@proteccion.com.co>
2023-05-05 09:40:31 -0500

    Globales.

--- node/fundamentos/modulos/globales.js
+++ node/fundamentos/modulos/globales.js
@@ -1,3 +1,21 @@
-global.mivariable = "Variable"
+// let i = 0;
+// let intervalo = setInterval(function(){
+//     console.log("HOla");
+//     if (i === 3){
+//         clearInterval(intervalo);
+//     }
+//     i++;    
+// }, 1000);
 
+// setImmediate(function(){
+//     console.log("Hola");
+// });
+
+// console.log(process);
+
+console.log(__dirname);
+console.log(__filename);
+
+// --varible global
+global.mivariable = "mivariable";
 console.log(mivariable)
